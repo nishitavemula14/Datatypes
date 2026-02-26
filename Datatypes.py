e263720c030644271c2d724c37f1341dff8df533
nishitavemula14 <nishita.vemula@fissionlabs.com>
2026-02-26 21:18:54 +0530

    Create Datatypes.py

---
+++ Datatypes.py
@@ -0,0 +1,29 @@
+# Integer example
+a = 10
+b = 5
+
+sum_result = a + b
+print("Sum:", sum_result)
+print("Type of a:", type(a))
+
+
+# Float example
+c=10.0
+d=2.0
+division = c/d
+print("Division",division)
+print(type(division))
+
+#String example
+name = "Alice"
+message = "Hello, " + name
+
+print(message)
+print("Length:", len(name))
+print("Type:", type(name))
+
+# Boolean example
+num = 10
+is_positive = num > 0
+print("Is positive?", is_positive)
+print("Type:", type(is_positive))
