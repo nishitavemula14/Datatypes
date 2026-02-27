0018bfd59a8d0092d7e03c2379b9f8fea017f817
nishitavemula14 <nishita.vemula@fissionlabs.com>
2026-02-27 13:10:14 +0530

    Add file

--- Dictionary.py
+++ Dictionary.py
@@ -1,19 +1,9 @@
 a = {
     "name":"John",
     "Age": 25,
-    "grade":"A"
+    "grade": "A"
 }
-print(a["name"])
-print(a.get("Age"))
-print(a.pop("grade"))
-new_data=a.copy()
-print(new_data)
-print(a.items())
-print(a.keys())
-print(a.values())
-value = (a.pop("Age"))
-print(value)
-print(a.popitem())
 print(a)
-a.update({"Age":25})
-print(a)
+
+for i in a:
+    print(a[i])
---
+++ tuple.py
@@ -0,0 +1,41 @@
+c = (10,20,50,90,72)
+print(c[0])
+   
+
+
+
+t1 =(10,50,30)
+t2 =(60,80,70)
+t3= t1+t2
+print(t3)
+
+t4 =(10,20)
+print(t4*3)
+
+t5 =(10,60,80)
+print(10 in t5)
+print(36 not in t5)
+
+t6 =(50,90,12,10)
+sorted_list = sorted(t6)
+print(sorted_list)
+print(max(t6))
+print(min(t6))
+print(len(t6))
+print(t6.count(90))
+print(t6[0])
+
+
+
+
+
+t7=(20,86,49,21,5,47)
+
+for i in t7:
+    print(i)
+
+t8 =(2,4,(3,6))
+print(t8[2])
+print(t8[2][0])
+
+
